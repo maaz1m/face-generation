fab5e4367be8a08ce49fce321be74346554a88e3
Mohammad Maaz <mohammadmaaz1997@gmail.com>
2022-12-08 15:53:08 -0500

    added skeleton for wgan, vae

--- test.py
+++ test.py
@@ -2,21 +2,30 @@ from tensorflow.keras.models import load_model
 from tensorflow.random import normal
 import matplotlib.pyplot as plt
 
-LATENT_DIM = 128
+model_type = 'gan'
 
-# load saved model
-model = load_model('saved/GAN/generator')
+if model_type == 'gan':
+    LATENT_DIM = 128
 
-# generate images from the generator of the loaded model
-random_latent_vectors = normal(shape=(10, LATENT_DIM))
-generated_images = model(random_latent_vectors)
-generated_images *= 255
+    # load saved model
+    model = load_model('saved/GAN/generator')
 
-# visualize the images
-plt.figure(figsize=(8, 4))
-for i in range(10):
-    ax = plt.subplot(2, 5, i + 1)
-    plt.imshow(generated_images[i].numpy().astype("uint8"))
-    plt.axis("off")
-plt.tight_layout()
-plt.show()
+    # generate images from the generator of the loaded model
+    random_latent_vectors = normal(shape=(10, LATENT_DIM))
+    generated_images = model(random_latent_vectors)
+    generated_images *= 255
+
+    # visualize the images
+    plt.figure(figsize=(8, 4))
+    for i in range(10):
+        ax = plt.subplot(2, 5, i + 1)
+        plt.imshow(generated_images[i].numpy().astype("uint8"))
+        plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+elif model_type == 'wgan':
+    1
+elif model_type == 'vae':
+    1
+else:
+    raise Exception('Model type not recognized')
--- train.py
+++ train.py
@@ -1,31 +1,40 @@
 import tensorflow as tf
-from models.GAN import GAN
+from models import GAN
 from callbacks import VisualizeImages
 
-LATENT_DIM = 128
-NUM_EPOCHS = 30
-BATCH_SIZE = 64
+model_type = 'gan'
 
-# load image files from directory as tensorflow Dataset object
-dataset = tf.keras.preprocessing.image_dataset_from_directory('data/img_align_celeba', label_mode=None, image_size=(64, 64), batch_size=BATCH_SIZE)
+if model_type == 'gan':
+    LATENT_DIM = 128
+    NUM_EPOCHS = 30
+    BATCH_SIZE = 64
+    # load image files from directory as tensorflow Dataset object
+    dataset = tf.keras.preprocessing.image_dataset_from_directory('data/img_align_celeba', label_mode=None, image_size=(64, 64), batch_size=BATCH_SIZE)
 
-# convert integer encodings to float
-dataset = dataset.map(lambda x: x / 255.)
+    # convert integer encodings to float
+    dataset = dataset.map(lambda x: x / 255.)
+    # create and compile model object
+    model = GAN(latent_dim=LATENT_DIM)
+    model.compile()
 
-# create and compile model object
-model = GAN(latent_dim=LATENT_DIM)
-model.compile()
+    # create callback for visualizing results
+    visualize_images = VisualizeImages(latent_dim=LATENT_DIM)
 
-# create callback for visualizing results
-visualize_images = VisualizeImages(latent_dim=LATENT_DIM)
+    # create callback for temporary saving model after every epoch, so if training
+    # is interrupted we can resume from the last completed epoch
+    backup = tf.keras.callbacks.BackupAndRestore(backup_dir="backup")
 
-# create callback for temporary saving model after every epoch, so if training
-# is interrupted we can resume from the last completed epoch
-backup = tf.keras.callbacks.BackupAndRestore(backup_dir="backup")
+    # train the model
+    hist = model.fit(dataset, epochs=NUM_EPOCHS, callbacks=[visualize_images, backup])
 
-# train the model
-hist = model.fit(dataset, epochs=NUM_EPOCHS, callbacks=[visualize_images, backup])
-
-# save model for further training or inference
-model.generator.save('saved/GAN/generator')
-model.discriminator.save('saved/GAN/discriminator')
+    # save model for further training or inference
+    model.generator.save('saved/GAN/generator')
+    model.discriminator.save('saved/GAN/discriminator')
+elif model_type == 'wgan':
+    # TODO Add WGAN train code
+    1
+elif model_type == 'vae':
+    # TODO Add VAE train code
+    1
+else:
+    raise Exception('Model type not recognized')
