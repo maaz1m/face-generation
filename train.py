5b5a2b24ea87c65cc5cb6a209be92504de003ef0
suhasburavalla <106994165+sburavalla@users.noreply.github.com>
2022-12-08 18:24:04 -0500

    Revert "WGAN Push"
    This reverts commit f011ecb124e26b8e5ce1f2cbe3ee2308bace4a85.

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from models import GAN
-from models import WGAN
 from callbacks import VisualizeImages
 
 model_type = 'gan'
@@ -33,32 +32,7 @@ if model_type == 'gan':
     model.discriminator.save('saved/GAN/discriminator')
 elif model_type == 'wgan':
     # TODO Add WGAN train code
-    LATENT_DIM = 128
-    NUM_EPOCHS = 30
-    BATCH_SIZE = 64
-    # load image files from directory as tensorflow Dataset object
-    dataset = tf.keras.preprocessing.image_dataset_from_directory('data/img_align_celeba', label_mode=None,
-                                                                  image_size=(64, 64), batch_size=BATCH_SIZE)
-
-    # convert integer encodings to float
-    dataset = dataset.map(lambda x: x / 255.)
-    # create and compile model object
-    model = WGAN(latent_dim=LATENT_DIM)
-    model.compile()
-
-    # create callback for visualizing results
-    visualize_images = VisualizeImages(latent_dim=LATENT_DIM)
-
-    # create callback for temporary saving model after every epoch, so if training
-    # is interrupted we can resume from the last completed epoch
-    backup = tf.keras.callbacks.BackupAndRestore(backup_dir="backup")
-
-    # train the model
-    hist = model.fit(dataset, epochs=NUM_EPOCHS, callbacks=[visualize_images, backup])
-
-    # save model for further training or inference
-    model.generator.save('saved/WGAN/generator')
-    model.discriminator.save('saved/WGAN/discriminator')
+    1
 elif model_type == 'vae':
     # TODO Add VAE train code
     1
